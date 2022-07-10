5d60f46671886b1746a467a99452502d88dabfaf
dmbackend <python.dev2@acquaintsoft.com>
2022-07-10 14:21:39 +0530

    user-detail implemented.

--- users_api/urls.py
+++ users_api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from users_api.views import InsertUsers, UserList
-
+from users_api.views import InsertUsersView, UserListView, UserDetailView
 app_name = "users_api"
 
 urlpatterns=[
-    path('insert', InsertUsers.as_view(), name='insert_users'),
-    path('api/users', UserList.as_view(), name='users_list')
+    path('insert', InsertUsersView.as_view(), name='insert_users'),
+    path('api/users', UserListView.as_view(), name='users_list'),
+    path('api/users/<int:pk>', UserDetailView.as_view(), name='user_detail')
 ]
--- users_api/views.py
+++ users_api/views.py
@@ -3,14 +3,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from users_api.models import User
 from users_api.serializers import UserSerializer
 import requests
 # Create your views here.
 
 
-class InsertUsers(CreateAPIView):
+class InsertUsersView(CreateAPIView):
     """
     This class is responsible for insert all user's data into the DB using postman.
     """
@@ -19,7 +19,7 @@ class InsertUsers(CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        many = isinstance(data, list)        
+        many = isinstance(data, list)
         serializer = self.get_serializer(data=data, many=many)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -30,7 +30,7 @@ class InsertUsers(CreateAPIView):
                 headers=headers
         )
 
-class UserList(ListAPIView):
+class UserListView(ListAPIView):
     """This class is responsible for showing the list of all users."""
 
     permission_classes = [AllowAny]
@@ -39,10 +39,25 @@ class UserList(ListAPIView):
     def get_queryset(self):
         queryset = User.objects.all()
         return queryset
-    
+
     def get(self, request, *args, **kwargs):
 
-        serializer = self.serializer_class(self.get_queryset() , many=True)        
+        serializer = self.serializer_class(self.get_queryset(),
+                                           many=True,                                           
+                                           )
+        return Response(
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+
+class UserDetailView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        obj = self.get_object()
+        serializer = self.serializer_class(instance=obj, context={'request': request})
         return Response(
             status=status.HTTP_200_OK,
             data=serializer.data
